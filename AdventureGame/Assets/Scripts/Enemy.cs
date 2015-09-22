869a2786648bcce474787e56cc6963775100bc72
KaniZ0r <the.heq@gmail.com>
2015-09-22 19:40:04 +0300

    0.11

--- AdventureGame/Assets/Scripts/Enemy.cs
+++ AdventureGame/Assets/Scripts/Enemy.cs
@@ -4,16 +4,28 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 
 	public Transform target;
+	public Animator anim;
 	public float moveSpeed;
+
+	float x;
+	float y;
 
 
 	// Use this for initialization
 	void Start () {
 		target = GameObject.FindWithTag ("Player").transform;
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		x = target.transform.position.x - transform.position.x;
+		y = target.transform.position.y - transform.position.y;
+
+		anim.SetFloat ("X", x);
+		anim.SetFloat ("Y", y);
+
 		transform.position += (target.transform.position - transform.position).normalized * moveSpeed * Time.deltaTime;
 	}
 
