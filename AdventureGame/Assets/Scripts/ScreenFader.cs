88b46e4ee4d2648d2a139ada06643b51711ad9ea
alppu2 <Aleksi.A.Valta@edu.savonia.fi>
2015-09-29 20:15:41 +0300

    0.15
    warp testi

---
+++ AdventureGame/Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenFader : MonoBehaviour {
+
+	Animator anim;
+	bool isFading;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	public IEnumerator FadeToClear() {
+		isFading = true;
+		anim.SetTrigger ("FadeIn");
+		anim.SetBool ("FadeOut", false);
+
+		while (isFading)
+			yield return null;
+	}
+
+	public IEnumerator FadeToBlack() {
+		isFading = true;
+		anim.SetTrigger ("FadeOut");
+		anim.SetBool ("FadeIn", false);
+
+		while (isFading)
+			yield return null;
+	}
+
+	void AnimationComplete(){
+		isFading = false;
+	}
+}
---
+++ AdventureGame/Assets/Scripts/Warp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warp : MonoBehaviour {
+
+	public Transform WarpTarger;
+	
+	IEnumerator OnTriggerEnter2D (Collider2D other) {
+		ScreenFader sf = GameObject.FindGameObjectWithTag ("Fader").GetComponent<ScreenFader> ();
+
+		yield return StartCoroutine (sf.FadeToBlack ());
+
+		other.gameObject.transform.position = WarpTarger.position;
+		Camera.main.transform.position = WarpTarger.position;
+
+		yield return StartCoroutine (sf.FadeToClear ());
+	}
+}
