6af3911e37f8a8efecb68a71588d51a3c428d9e2
KaniZ0r <the.heq@gmail.com>
2015-10-07 10:33:08 +0300

    0.200
    NPC Dialog systeemi

--- AdventureGame/Assets/Scripts/npc_script.cs
+++ AdventureGame/Assets/Scripts/npc_script.cs
@@ -5,25 +5,48 @@ public class npc_script : MonoBehaviour {
 
 	public GUIStyle customGuiStyle;
 	public Sprite oldmanHead;
-	string text;
-	string name1;
-
+	bool readed = false;
+	public int page = 1;
 	public GameObject textbox;
+	public TextAsset textfile1;
+	public TextAsset textfile2;
+	Animator anim;
 
-	Vector3 screenpos;
+	void Update () {
+
+		anim = GetComponent<Animator> ();
+
+		FindObjectOfType<ImageScript> ().SetImage (oldmanHead);
+		if (page == 1) {
+			FindObjectOfType<TextScript> ().SetText (textfile1.text);
+		} else {
+			FindObjectOfType<TextScript> ().SetText (textfile2.text);
+		}
+	}
 
 	void OnTriggerStay2D(Collider2D Other){
 		if (Other.tag == "Player") {
-			textbox.SetActive(true);
-			text = "Haluan vain kertoa, että Alppu2 on vitun homoneekerin mätäpaska!!½";
-			FindObjectOfType<TextScript>().SetText(text);
-			FindObjectOfType<ImageScript>().SetImage(oldmanHead);
-		}
+			if (readed == false) {
+				FindObjectOfType<PlayerController> ().Freeze ();
+				textbox.SetActive (true);
+			}
+			if (Input.GetKeyDown (KeyCode.Space) && readed == true) {
+				FindObjectOfType<PlayerController> ().UnFreeze ();
+				textbox.SetActive (false);
+				anim.SetBool("Died", true);
+			}
+			if (Input.GetKeyDown (KeyCode.Space) && page == 1) {
+				page = 2;
+				readed = true;
+			}
 
+		}
 	}
-	void OnTriggerExit2D(Collider2D Other){
+
+	void OnTriggerExit2D(Collider2D Other) {
 		if (Other.tag == "Player") {
-			textbox.SetActive(false);
+			readed = false;
+			page = 1;
 		}
 	}
 }
