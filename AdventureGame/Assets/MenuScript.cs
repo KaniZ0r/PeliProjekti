cd16d9b78ae118b6769f256ab2dcea354878fd3d
Utiz <utistos@hotmail.com>
2015-10-21 10:33:45 +0300

    0.204
    ebinmenu

---
+++ AdventureGame/Assets/MenuScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public void StartLevel(){
+		Application.LoadLevel ("DevZone 1");
+	}
+
+	public void Exit() {
+		Application.Quit ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
