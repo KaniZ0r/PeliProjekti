248cfea6400fcece789f5a3ba65aa07376ff5275
KaniZ0r <the.heq@gmail.com>
2015-11-08 15:32:25 +0200

    0.210
    Guardia ja dialogia aloiteltu

---
+++ AdventureGame/Assets/Scripts/CanvasController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasController : MonoBehaviour {
+		
+	public GameObject Dialog;
+	float timer;
+	float timer1;
+	bool itsActive;
+
+	void Start () {
+		timer = 2;
+		itsActive = false;
+	}
+
+
+	void Update () {
+		if (Dialog) {
+			itsActive = true;
+		}
+		if (itsActive) {
+			timer1 = 0;
+			if (timer1 < timer) {
+				timer += Time.deltaTime;
+			} else {
+				Dialog.SetActive(false);
+				itsActive = false;
+			}	
+		}
+	}
+}
---
+++ AdventureGame/Assets/Scripts/Guard.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Guard : MonoBehaviour {
+	
+	public Sprite face;
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Player") {
+			FindObjectOfType<CanvasController> ().Dialog.SetActive (true);
+		}
+	}
+	void OnTriggerStay2D (Collider2D other) {
+			if (other.tag == "Player") {
+			FindObjectOfType<TextScript> ().SetText ("Et voi kulkea tästä");
+			FindObjectOfType<ImageScript> ().SetImage (face);
+			FindObjectOfType<PlayerController>().knockBack(transform.position.x, transform.position.y);
+		}
+	}
+}
