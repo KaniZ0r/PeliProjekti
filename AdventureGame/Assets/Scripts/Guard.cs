8f511aace7d89d96447059f8536fbd00ea9a256f
alppu2 <Aleksi.A.Valta@edu.savonia.fi>
2015-11-10 18:02:33 +0200

    0.226
    Guardeille push anim

--- AdventureGame/Assets/Scripts/Guard.cs
+++ AdventureGame/Assets/Scripts/Guard.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class Guard : MonoBehaviour {
 	
 	public Sprite face;
+	Animator anim;
 
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.tag == "Player") {
 			FindObjectOfType<CanvasController> ().Dialog.SetActive (true);
+			anim = GetComponent<Animator>();
 		}
 	}
 	void OnTriggerStay2D (Collider2D other) {
@@ -15,6 +17,7 @@ public class Guard : MonoBehaviour {
 			FindObjectOfType<TextScript> ().SetText ("Et voi kulkea tästä");
 			FindObjectOfType<ImageScript> ().SetImage (face);
 			FindObjectOfType<PlayerController>().knockBack(transform.position.x, transform.position.y);
+			anim.SetTrigger("hit");
 		}
 	}
 }
