bde8ed66612b0d57d9fdc3b51b1626e1798806a4
alppu2 <Aleksi.A.Valta@edu.savonia.fi>
2015-11-22 18:15:26 +0200

    0.229
    Kartan eka alue lähes valmis ja importattu unityyn. Ekan metsäbossin
    scriptiä alotettu.

---
+++ AdventureGame/Assets/Scripts/Boss_ManEaterFlower.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss_ManEaterFlower : MonoBehaviour {
+
+	public Transform target;
+	public GameObject projectile;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Instantiate (projectile, this.transform.position, this.transform.rotation);
+
+	}
+}
---
+++ AdventureGame/Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileController : MonoBehaviour {
+
+	public float speed;
+	Rigidbody2D rb2d;
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb2d.velocity = new Vector2 (speed, rb2d.velocity.y);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		anim.SetBool("hit", true);
+		if (other.tag == "Player") {
+			FindObjectOfType<PlayerController>().takeDamage();
+		}
+		Destroy (gameObject);
+	}
+}
