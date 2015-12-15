2fc452fd0847cbe51b45801cbf9e0fc1fbedcf76
KaniZ0r <the.heq@gmail.com>
2015-12-15 09:19:50 +0200

    0.239
    GUI updatea ja uusi menu

---
+++ AdventureGame/Assets/Scripts/Intro.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour {
+	public GameObject introbg;
+	public GameObject introtext;
+
+	// Use this for initialization
+	void Start () {
+		introbg.SetActive (true);
+		StartCoroutine (IntroGo());
+		FindObjectOfType<PlayerController> ().Freeze ();
+	}
+
+	IEnumerator IntroGo () {
+		ScreenFader sf = GameObject.FindGameObjectWithTag ("Fader").GetComponent<ScreenFader> ();
+		introtext.GetComponent<TextScript> ().SetText ("World is a dire place...");
+		yield return new WaitForSeconds(3f);
+		introtext.GetComponent<TextScript> ().SetText ("It became very clear to our hero at young age...");
+		yield return new WaitForSeconds(3f);
+		introtext.GetComponent<TextScript> ().SetText ("His parents lives were taken by wicked man...");
+		yield return new WaitForSeconds(3f);
+		introtext.GetComponent<TextScript> ().SetText ("This wayfarer of misery left our boy alive...");
+		yield return new WaitForSeconds(3f);
+		introtext.GetComponent<TextScript> ().SetText ("But was it a mistake?");
+		yield return new WaitForSeconds(3f);
+		yield return StartCoroutine(sf.FadeToBlack());
+		introbg.SetActive (false);
+		yield return StartCoroutine (sf.FadeToClear());
+		FindObjectOfType<PlayerController> ().UnFreeze ();
+		Destroy (gameObject);
+	}
+}
--- AdventureGame/Assets/Scripts/MenuScript.cs
+++ AdventureGame/Assets/Scripts/MenuScript.cs
@@ -3,21 +3,55 @@ using System.Collections;
 
 public class MenuScript : MonoBehaviour {
 
-	public void StartLevel(){
-		Application.LoadLevel ("Level1");
-	}
-
-	public void Exit() {
-		Application.Quit ();
-	}
+	public GameObject startGame;
+	public GameObject exitGame;
+	public GameObject options;
+	public Sprite img1;
+	public Sprite img2;
+	int currentObject;
 
 	// Use this for initialization
 	void Start () {
-	
+		currentObject = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		switch (currentObject) {
+		case 1:
+			startGame.GetComponent<ImageScript>().SetImage(img2);
+			exitGame.GetComponent<ImageScript>().SetImage(img1);
+			options.GetComponent<ImageScript>().SetImage(img1);
+			if (Input.GetKeyDown(KeyCode.UpArrow)){
+				currentObject = 2;
+			} else if (Input.GetKeyDown(KeyCode.DownArrow)){
+				currentObject = 3;
+			} else if (Input.GetKeyDown (KeyCode.Return)){
+				Application.LoadLevel ("Level1");
+			}
+			break;
+		case 2:
+			startGame.GetComponent<ImageScript>().SetImage(img1);
+			exitGame.GetComponent<ImageScript>().SetImage(img2);
+			options.GetComponent<ImageScript>().SetImage(img1);
+			if (Input.GetKeyDown(KeyCode.UpArrow)){
+				currentObject = 3;
+			} else if (Input.GetKeyDown(KeyCode.DownArrow)){
+				currentObject = 1;
+			}
+			break;
+		case 3:
+			startGame.GetComponent<ImageScript>().SetImage(img1);
+			exitGame.GetComponent<ImageScript>().SetImage(img1);
+			options.GetComponent<ImageScript>().SetImage(img2);
+			if (Input.GetKeyDown(KeyCode.UpArrow)){
+				currentObject = 1;
+			} else if (Input.GetKeyDown(KeyCode.DownArrow)){
+				currentObject = 2;
+			}
+			break;
+		}
+
+
 	}
 }
